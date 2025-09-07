20638e76ad539a254b88a210657d53773fbd12c6
Mohammad Sami Chowdhury <contact.mohammadsami@gmail.com>
2025-09-08 01:45:32 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import PageOne from "./pages/PageOne";
 import PageTwo from "./pages/PageTwo";
+import Countdown from "./pages/CountDown";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/page-one",
     element: <PageOne />,
   },
   {
     path: "/page-two",
     element: <PageTwo />,
+  },
+  {
+    path: "/",
+    element: <Countdown />,
   },
 ]);
 
